01b9b5104f6ff52f0d0f3e8046f4a1b3cdbd3c50
jeremyzetadev <jeremy_zeta@yahoo.com>
2026-09-14 04:07:57 +0900

    add sample2

---
+++ sample2/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const firstname = "John";
+const lastname = "Doe";
+console.log(firstname + " " + lastname);
+const num1 = 11;
+const num2 = 22;
+console.log(num1 + num2);
---
+++ sample2/index.ts
@@ -0,0 +1,9 @@
+const firstname : string = "John";
+const lastname : string = "Doe";
+
+console.log(firstname + " " + lastname);
+
+const num1 : number = 11;
+const num2 : number = 22;
+
+console.log(num1 + num2);
