15083c598c2829b9359ca4c02100eba64b8cd64c
jeremyzetadev <jeremy_zeta@yahoo.com>
2026-09-13 18:22:08 +0900

    sample1 folder add

---
+++ sample1/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const num = 100;
+console.log("The num is: " + num);
+const char = "Z";
+console.log("The chas is: " + char);
+//# sourceMappingURL=index.js.map
---
+++ sample1/index.ts
@@ -0,0 +1,5 @@
+const num: any = 100;
+console.log("The num is: " + num);
+
+const char: string = "Z";
+console.log("The chas is: " + char);
